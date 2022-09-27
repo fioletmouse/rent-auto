dfa773e25945cfc210ad41403f427c2f619520cf
Evgeniya Akulenko <eakulenko@4xxi.com>
2022-09-27 12:52:31 +0300

    utils functions

--- src/bl/bl.service.ts
+++ src/bl/bl.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import * as moment from 'moment';
 
 @Injectable()
 export class BLService {
   constructor() {}
 
-  IsMaxDayLimit = () => {
-    return false;
+  moreThanDayLimit = (start: Date, end: Date) => {
+    const mEnd = moment(end);
+    const mStart = moment(start);
+    const daysCount =  mEnd.diff(mStart, 'days') + 1;
+    return daysCount > 30;
+  }
+
+  private isWeekendDay = (date: Date) => {
+    const weekDayNum = moment(date).day();
+    return weekDayNum === 6 || weekDayNum === 0 // Sat || Sun;
+  }
+
+  isWeekendDates = (start: Date, end: Date) => {
+    return this.isWeekendDay(start) || this.isWeekendDay(end);
   }
 }
